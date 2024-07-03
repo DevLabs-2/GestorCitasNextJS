9e633a3e98940d585a7f22b980d1fb64385b877e
Android Studio <devandroid@ort>
2024-07-03 14:37:30 -0300

    3/7 falta importar css

---
+++ gestorcitasnextjs/src/app/page.js
@@ -0,0 +1,43 @@
+'use client'
+import Listado from '../components/Listado/index.jsx'
+import Formulario from '../components/Formulario/Index.jsx';
+import { useState } from 'react';
+import Modal from '../components/Modal/index.jsx';
+export default function Home() {
+const [arrayTurnos, setArrayturnos] = useState([]);
+const addCita = (cita) => {
+  setArrayturnos([...arrayTurnos, cita]);
+};
+
+const [turnoAEliminar, setTurnoAEliminar] = useState();
+const [showModal, setShowModal] = useState(false);
+
+const deleteCita = (target) => {
+  setTurnoAEliminar(target)
+  setShowModal(true)
+};
+
+const deletionHandler = (target) =>{
+  let mascotaName = target.target.id
+  const updatedArray = arrayTurnos.filter(cita => cita.mascotaName !== mascotaName);
+  setArrayturnos(updatedArray);
+  setShowModal(false)
+}
+
+const deleteString = 'Vas a eliminar esta cita. Deseas continuar?'
+
+  return (
+    <div className="App"> 
+      <Formulario onAddCita={addCita}/>
+      <Listado arrayTurnos = {arrayTurnos} onDeleteCita = {deleteCita}/>
+      {showModal && <Modal h1={deleteString} setter={(value) => {
+            if (value) {
+              deletionHandler(turnoAEliminar); 
+            }
+            setShowModal(false); 
+          }}
+          isOpen={setShowModal}
+      />}
+    </div>
+  );
+}
